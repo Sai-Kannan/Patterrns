c030412e266b02685fb75451e65467fc6fbb9ff2
Sai kannan <kannanking545@gmail.com>
2021-08-23 22:21:46 +0530

    Patterns of different style

---
+++ patterns.c
@@ -0,0 +1,110 @@
+// to print 1 22 333 4444 5555
+#include<stdio.h>
+int main()
+{
+	int n=5,i,j;
+	/*for(i=1;i<=5;i++)
+	{
+		for(j=i;j<5;j++)
+		printf(" ");
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",i);
+		}printf("\n");
+	}
+	
+	to print 1 12 123 1234 12345
+	
+		int n=5,i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=i;j<5;j++)
+		printf(" ");
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j);
+		}printf("\n");
+	}
+	to print 1 23 345 567 8910 1112131415
+	
+		for(i=1;i<=5;i++)
+	{
+		for(j=i;j<5;j++)
+		printf(" ");
+		for(j=1;j<=i;j++)
+		{
+			n++;
+			printf("%d ",n);
+		}printf("\n");
+	}
+	to print * ** *** **** *****
+	
+		for(i=1;i<=5;i++)
+	{
+		for(j=i;j<5;j++)
+		printf(" ");
+		for(j=1;j<=i;j++)
+		{
+			n++;
+			printf("* ",n);
+		}printf("\n");
+	}
+	
+	to print 12345 1234 123 12 1
+	
+	
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j);
+		}printf("\n");
+	}
+    
+    to print ***** **** *** ** *
+    
+    for(i=5;i>=1;i--)
+	{
+		for(j=i;j<=5;j++)
+		printf(" ");
+		for(j=i;j>=1;j--)
+		{
+			printf("* ");
+		}printf("\n");
+	}
+	
+	to print * ** *** **** ***** **** *** ** *
+	
+	for(i=5;i>=1;i--)
+	{
+		for(n=5;n>=i;n--)
+		printf("* ");
+		printf("\n");
+	}
+	for(i=4;i>=1;i--)
+	{
+	for(j=1;j<=i;j++)
+		{
+			printf("* ");
+		}printf("\n");
+    }
+    
+    //to print * ** *** **** ***** **** *** ** *
+    	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<i;j++)
+		printf(" ");
+		for(n=5;n>=i;n--)
+		printf("*");
+		printf("\n");
+	}
+	for(i=4;i>=1;i--)
+	{
+		for(j=4;j>=i;j--)
+		printf(" ");
+	for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}printf("\n");
+    }*/
+}:
